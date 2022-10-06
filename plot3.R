aada6a54f551cf3b4cbfe2ff0cb5cb8949932c99
lulupanda <113456922+lulupanda@users.noreply.github.com>
2022-10-06 11:37:30 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,4 +1,4 @@
-dataFile <- "/Users/lululi/Downloads/household_power_consumption.txt"
+dataFile <- "~/Downloads/household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
@@ -15,4 +15,3 @@ lines(datetime, subMetering2, type="l", col="red")
 lines(datetime, subMetering3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 dev.off()
-
